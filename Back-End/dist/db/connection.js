c57ac7a13bbc83f80cab3a735018329649e61b9a
Jeremias <123414243+LombardiniJere@users.noreply.github.com>
2023-09-08 08:25:11 -0300

    Delete Back-End/dist directory
    Delete Back-End/dist directory, Carpeta dist borrada, se crea con el comando npm run build

--- Back-End/dist/db/connection.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-// Conexión a la BD MySQL
-const sequelize = new sequelize_1.Sequelize('appmina', 'root', 'adminadmin', {
-    host: "localhost",
-    dialect: "mysql",
-});
-exports.default = sequelize;
