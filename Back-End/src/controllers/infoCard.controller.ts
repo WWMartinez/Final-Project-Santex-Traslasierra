1629e3542267248bb7888220480d696a2d84b89c
LombardiniJere <jere.lombardini@gmail.com>
2023-09-20 17:20:34 -0300

    server cloudinary setup2

---
+++ Back-End/src/controllers/infoCard.controller.ts
@@ -0,0 +1,12 @@
+import InfoCard from "../models/infoCard.model";
+import cloudinary from "../utils/cloudinary";
+
+export const infoCard = async (req, res, next ) => {
+  const { image, title, description, category } = req.body;
+
+  try {
+    
+  } catch (error) {
+    
+  }
+};
--- Back-End/src/models/infoCard.model.ts
+++ Back-End/src/models/infoCard.model.ts
@@ -2,20 +2,30 @@ import { DataTypes } from 'sequelize';
 const sequelize = require('../config/connection');
 
 export const InfoCard = sequelize.define('infoCard', {
-    id: {
+  id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
+  },
+  image: {
+    public_id: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    image: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    titulo: {
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+    title: {
         type: DataTypes.STRING,
         allowNull: false
     },
     description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    category: {
         type: DataTypes.STRING,
         allowNull: false
     },
