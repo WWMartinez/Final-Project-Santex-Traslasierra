b84680b7defd642d363fb297a0b38b4639056ac8
LombardiniJere <jere.lombardini@gmail.com>
2023-10-03 07:38:07 -0300

    DB association Pregunta-hasMany-Respuesta

--- Back-End/src/routes/respuesta.route.js
+++ Back-End/src/routes/respuesta.route.js
@@ -1,11 +1,13 @@
 const { Router } =require('express');
 const router = Router();
-const { createRespuesta, getIdRespuesta, findRespuestas, getResOfPre, putRespuesta, deleteRespuesta } = require('../controllers/respuesta.controller');
+const { createRespuesta, getIdRespuesta, findRespuestas, putRespuesta, deleteRespuesta } = require('../controllers/respuesta.controller');
 
 router.post('/', createRespuesta);
-// router.get('/:respuestaId', getIdRespuesta);
+router.get('/:respuestaId', getIdRespuesta);
 router.get('/', findRespuestas);
-router.get('/:respuestaId', getResOfPre);
+// route de FindRespuestas associated to pregunta
+// router.get('/:respuestaId', getResOfPre);
+
 router.put('/:respuestaId', putRespuesta);
 router.delete('/:respuestaId', deleteRespuesta);
 
