1beca4314f346fe64dfc702b40c25a7e47cf0852
LombardiniJere <jere.lombardini@gmail.com>
2023-10-03 15:30:44 -0300

    userService added

---
+++ Back-End/src/services/user.ser.js
@@ -0,0 +1,29 @@
+const { userProvider } = require('../providers');
+
+const createUser = async (user) => {
+  return await userProvider.createUser(user);
+};
+
+const getIdUser = async (id) => {
+  return await userProvider.getIdUser(id);
+};
+
+const findUsers = async (options) => {
+  return await userProvider.findUsers(options);
+};
+
+const putUser = async (id, user) => {
+  return await userProvider.putUser(id, user);
+};
+
+const deleteUser = async (id) => {
+  return await userProvider.deleteUser(id);
+};
+
+module.exports = { 
+  createUser,
+  getIdUser,
+  findUsers,
+  putUser,
+  deleteUser,
+};
