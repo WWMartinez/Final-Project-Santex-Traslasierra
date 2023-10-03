1b0bbbddf7123e7b7d1164aad53a633cd725f529
LombardiniJere <jere.lombardini@gmail.com>
2023-10-03 15:26:32 -0300

    userProvider added

--- Back-End/src/providers/user.provider.js
+++ Back-End/src/providers/user.provider.js
@@ -1,8 +1,9 @@
-const { adminModel } =require("../models");
+const UserModel = require('../models/user.model');
+const AdminModel = require('../models/admin.model');
 
 const validateUser = async (username) => {
   try {
-    const user = await adminModel.findOne({ where: { username: username }, });
+    const user = await AdminModel.findOne({ where: { username: username }, });
     if (user) {
       return user;
     } else {
@@ -16,26 +17,70 @@ const validateUser = async (username) => {
 };
 
 
-// export const createUser = async (userOptions: any) => {
-//   try {
-//     const newUser = await Admin.create(userOptions);
-//     return newUser;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+// CREATE USER
+const createUser = async (userOptions) => {
+  try {
+    const newUser = await UserModel.create(userOptions);
+    return newUser;
+  } catch (error) {
+    throw error;
+  }
+};
 
-// export const getUser = async (id: any) => {
-//   try {
-//     const user = await Admin.findByPk(id, { include: [{ all: true }] });
-//     if (user) {
-//       return user;
-//     } else {
-//       throw new Error("User not found");
-//     }
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+// GET USER BY ID
+const getIdUser = async (id) => {
+  try {
+    const user = await UserModel.findByPk(id, { include: [{ all: true }] });
+    if (user) {
+      return user;
+    } else {
+      throw new Error("User not found");
+    }
+  } catch (error) {
+    throw error;
+  }
+};
 
-module.exports = { validateUser };
+// GET ALL USERs 
+const findUsers = async (options) => {
+  try {
+    const users = await UserModel.findAll(options);
+    // const usersArray = Array.isArray(users) ? users : [users];
+    // console.log(users);
+    return users;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// UPDATE USER BY ID
+const putUser = async (userId, userOptions) => {
+  try {
+    await getIdUser(userId);
+    const [numRowsUpdated] = await UserModel.update(userOptions, {
+      where: { id: userId },
+    });
+    console.log(`${numRowsUpdated} rows updated on DB`);
+    return UserModel.findByPk(userId);
+  } catch (error) {
+    throw error;
+  }
+};
+
+// DELETE USER
+const deleteUser = async (userId) => {
+  try {
+    return UserModel.destroy({ where: { id: userId } });
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  validateUser,
+  createUser,
+  getIdUser,
+  findUsers,
+  putUser,
+  deleteUser,
+};
